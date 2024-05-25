c8d22607cb298f542f15a966067a36262c4373e4
Exvigilare Gemini <dubois.adelin@gmail.com>
2024-05-25 18:20:39 +0200

    Style (rootlayout + news) : adding basic style

--- app/ui/news/newPresentation.tsx
+++ app/ui/news/newPresentation.tsx
@@ -6,10 +6,12 @@ export default function NewPresentation(newInfo: {
   resume: string;
 }) {
   return (
-    <div>
-      <Image src={imageInfo} alt="Info Image" width={500} height={500} />
-      <h1>{newInfo.title}</h1>
-      <p>{newInfo.resume}</p>
+    <div className="flex flex-col md:flex-row m-4 bg-gray-300 bg-opacity-20 p-4 rounded hover:bg-opacity-40 transition-all duration-250 cursor-pointer">
+      <Image src={imageInfo} alt="Info Image" width={300} height={500}  className="hidden md:block mr-4" />
+      <div >
+        <h1 className="text-xl font-bold ">{newInfo.title}</h1>
+        <p>{newInfo.resume}</p>
+      </div>
     </div>
   );
 }
--- app/ui/news/newsPresentation.tsx
+++ app/ui/news/newsPresentation.tsx
@@ -1,11 +1,16 @@
 const { news } = require("../../lib/placeholder-news");
 import NewPresentation from "./newPresentation";
 
-
 export default function NewsPresentation() {
-    console.log(news)
-    const newsList = news.map((info:{id: number, title: string, resume: string}) => <NewPresentation key={info.id} title={info.title} resume={info.resume}></NewPresentation>)
-  return (
-    <>{newsList}</>
+  console.log(news);
+  const newsList = news.map(
+    (info: { id: number; title: string; resume: string }) => (
+      <NewPresentation
+        key={info.id}
+        title={info.title}
+        resume={info.resume}
+      ></NewPresentation>
+    )
   );
+  return <>{newsList}</>;
 }
